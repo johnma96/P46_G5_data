2034dc183e6513abe93ccace275bc5db0aecdb1d
John Montoya <jmmontoyaz@unal.edu.co>
2021-10-09 19:38:30 -0500

    Script para creación de database en psql

---
+++ covid_db.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE covid_db;
+CREATE SCHEMA registro_pruebas_data;
+SET search_path TO registro_pruebas_data, public;
+
+CREATE TABLE departamento(
+    dep_id SERIAL PRIMARY KEY,
+    dep_nombre VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE ips(
+    ips_id SERIAL PRIMARY KEY,
+    ips_nombre VARCHAR(45) NOT NULL,
+    ips_clave VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE dep_ips(
+    depips_id SERIAL PRIMARY KEY,
+    dep_id_fk INT NOT NULL,
+    ips_id_fk INT NOT NULL,
+    FOREIGN KEY (dep_id_fk) REFERENCES departamento(dep_id),
+    FOREIGN KEY (ips_id_fk) REFERENCES ips(ips_id)
+);
+
+CREATE TABLE registros(
+    reg_id SERIAL PRIMARY KEY,
+    depips_id_fk INT NOT NULL,
+    reg_fecha TIMESTAMP DEFAULT NOW(),
+    reg_pruebas_positivas INT,
+    reg_pruebas_negativas INT,
+    reg_pruebas_indeterminandas INT,
+    reg_totalpruebas INT,
+    FOREIGN KEY (depips_id_fk) REFERENCES dep_ips(depips_id)   
+);
