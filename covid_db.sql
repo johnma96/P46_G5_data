b575504c865180174fae05eca0ad06a548d3c5d7
John Montoya <jmmontoyaz@unal.edu.co>
2021-10-13 07:59:51 -0500

    Casos de uso y ejemplos de queries

--- covid_db.sql
+++ covid_db.sql
@@ -33,10 +33,60 @@ CREATE TABLE dep_ips(
 CREATE TABLE registros(
     reg_id SERIAL PRIMARY KEY,
     depips_id_fk INT NOT NULL,
-    reg_fecha TIMESTAMP DEFAULT NOW(),
-    reg_pruebas_positivas INT,
-    reg_pruebas_negativas INT,
-    reg_pruebas_indeterminandas INT,
-    reg_totalpruebas INT,
+    fecha TIMESTAMP DEFAULT NOW(),
+    pruebas_positivas INT,
+    pruebas_negativas INT,
+    pruebas_indeterminandas INT,
+    totalpruebas INT,
     FOREIGN KEY (depips_id_fk) REFERENCES dep_ips(depips_id)   
 );
+
+--CASOS DE USO
+
+--Incersiones de prueba elementos de prueba
+INSERT INTO ips(ips_nombre, ips_clave) VALUES('colsanitas', 'colsanitas123');
+INSERT INTO ips(ips_nombre, ips_clave) VALUES('sura', 'sura3454');
+INSERT INTO ips(ips_nombre, ips_clave) VALUES('comfama envigado', 'cfE125');
+
+INSERT INTO departamento(dep_nombre) VALUES('Antioquia');
+INSERT INTO departamento(dep_nombre) VALUES('Nariño');
+INSERT INTO departamento(dep_nombre) VALUES('Huila');
+
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(1, 3);
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(1, 1);
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(3, 1);
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(2, 1);
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(2, 2);
+INSERT INTO dep_ips(dep_id_fk, ips_id_fk) VALUES(2, 3);
+
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(1, 40, 30, 15, 85);
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(1, 40, 30, 15, 85);
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(1, 20, 30, 40, 90);
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(3, 20, 45, 5, 70);
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(6, 10, 10, 10, 30);
+
+--Esta inserción debe fallar porque viola la llave foránea
+INSERT INTO registros(depips_id_fk, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas) 
+VALUES(7, 5, 45, 15, 65);
+
+--Ejemplo uniendo tablas para ver datos del departamento y su ips
+SELECT depips_id, dep_id, ips_id, dep_nombre, ips_nombre, ips_clave 
+FROM dep_ips 
+JOIN ips 
+ON dep_ips.ips_id_fk = ips.ips_id 
+JOIN departamento
+ON dep_ips.dep_id_fk = departamento.dep_id
+WHERE dep_nombre = 'Antioquia';
+
+--Ejemplo que une la tabla de registro con las demás tablas
+echa, depips_id, dep_nombre, ips_nombre, ips_clave, pruebas_positivas, pruebas_negativas,pruebas_indeterminandas, totalpruebas
+FROM registros
+JOIN dep_ips ON registros.depips_id_fk = dep_ips.depips_id
+JOIN ips ON dep_ips.ips_id_fk = ips.ips_id 
+JOIN departamento ON dep_ips.dep_id_fk = departamento.dep_id
+WHERE dep_nombre = 'Antioquia';
